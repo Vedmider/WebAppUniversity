d543dd00b7783ed9cb2b9ec504131b0a3edd9fe4
andrii.lisovets <andrii.lisovets@gmail.com>
2019-07-10 10:14:29 +0300

    add spring boot

--- src/main/java/com/study/projects/spring/controller/UniversityController.java
+++ src/main/java/com/study/projects/spring/controller/UniversityController.java
@@ -2,25 +2,39 @@ package com.study.projects.spring.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.study.projects.domain.UniversityManager;
 import com.study.projects.domain.University;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UniversityController {
+
+	@Autowired
+	private UniversityManager manager;
+
+	@Bean
+	UniversityManager manager(){
+		return new UniversityManager();
+	}
 	
-	private UniversityManager manager = new UniversityManager();
-	
-	@RequestMapping(value="/manager", method=RequestMethod.GET)
-    public String getOrderPage(Model model) {
+	@GetMapping("/manager")
+    public String getUniversitiesListPage(Model model) {
 		
 		ArrayList<University> universityList = manager.getAll();
 		model.addAttribute("universities", universityList);
-        return "manager/manager-list";
+        return "manager-list";
     }
+
+	@GetMapping("/")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 
 }
